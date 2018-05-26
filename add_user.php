e1141359a6b981fe994e85a374184ba291ef6a57
MagdaKowara <kowara.magda@gmail.com>
2018-05-26 13:58:24 +0200

    sfd

--- add_user.php
+++ add_user.php
@@ -11,8 +11,18 @@ $name = $_POST['name'];
 $surname=$_POST['surname'];
 $login=$_POST['login'];
 $pass=$_POST['pass'];
-$sql = 'INSERT INTO '.$table.' values(null,"'.$name.'","'.$surname.'","'.$login.'","'.$pass.'")';
-$result = mysql_query($sql) or die (mysql_error());
+
+//Walidacja
+$sql = 'SELECT * FROM '.$table. ' WHERE "login" = ' .$login; 
+$result = mysql_query($sql);
+if (count(mysql_fetch_array($result))) {
+    mysql_close($handleSDB);
+   // die ("W bazie już istnieje użytkownik o podanym loginie!");
+    header ('Location: ' . $_SERVER['HTTP_REFERER']);
+} else {
+    $sql = 'INSERT INTO '.$table.' values(null,"'.$name.'","'.$surname.'","'.$login.'","'.$pass.'")';
+    $result = mysql_query($sql) or die (mysql_error());
+}
 mysql_close($handleSDB);
 }
 
