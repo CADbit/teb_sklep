5f65bbe5624b591698fac5c784d98708df73fd30
Michał Kazula <michal.kazula@sulimo.pl>
2018-05-26 12:50:34 +0200

    TEB - sklep

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Michał Kazula
+ * Date: 2018-05-26
+ * Time: 09:24
+ */
+
+return [
+    'DB_HOST' => 'localhost',
+    'DB_USER' => 'teb',
+    'DB_PASS' => 'zaq1@WSX',
+    'DB_NAME' => 'teb_sklep'
+];
+
+?>
